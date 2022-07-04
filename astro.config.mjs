6c8290838699eb5fe2d15d44c69ee9542a1cf41d
Sasha Koss <koss@nocorp.me>
2022-07-04 22:06:47 +0800

    Integrate Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   outDir: "./dist/production/hosting",
+  integrations: [tailwind()]
 });
